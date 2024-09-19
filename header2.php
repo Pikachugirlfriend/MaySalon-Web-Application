c4da710efbb999a41d6b229ac2b76ad3ad7223d2
queenny0724 <queenny050724@gmail.com>
2024-09-19 21:40:02 +0800

    Create header2.php

---
+++ header2.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="header_style.css">
+    </head>
+<body>
+<header>
+            <div class="nav nav-container">
+                <a href="" class="logo">May Salon</a>
+                <div class="navbar">
+                    <a href="index2.php" class="nav-link">Home</a>
+                    <a href="aboutus.php" class="nav-link">About Us</a>
+                    <a href="FAQs.html" class="nav-link">Services</a>
+                    <a href="Drinks Menu.html" class="nav-link">Contact Us</a>
+                    <a href="mp_rewards.html" class="nav-link">Feedback</a>
+                    <a href="upload_profile.php">
+    <img src="maysalonprofilepicture.jpg" alt="Profile" class="profile-img">
+</a>
+                </div>
+                
+            </div>
+</header>
+
+<style>
+    .profile-img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%; /* Makes the image circular */
+        object-fit: cover;
+    }
+</style>
+
+</body>
+</html>
